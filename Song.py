ce93915e49dafd40156c5f42b53c02694a83e3da
lenbo <allenbora@gmail.com>
2023-01-30 15:33:21 +0800

    Twenty-Nineth

---
+++ Song.py
@@ -0,0 +1,22 @@
+def title():
+  text = """\n\n\nMaybe this time"""
+  
+  print(text)
+
+def verse1():
+  text = """
+  Maybe this time
+  It'll be lovin' they'll find
+  Maybe now they can be more.    
+  than just friends
+  She's back in his life
+  And it feels so right
+  Maybe this time, love won't end
+  """
+
+  print(text)
+
+def main():
+    title()#Maybe this time
+    verse1()#1
+main()
